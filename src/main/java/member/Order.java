40f5982b0626c11b192f9a95ca6fe4c8af381b39
Minwoo Jhung <minwoorich@naver.com>
2023-10-27 16:02:16 +0900

    [팀원] 주문하기 기능입니다

--- src/main/java/member/Order.java
+++ src/main/java/member/Order.java
@@ -2,6 +2,6 @@ package member;
 
 public class Order {
     public static void main(String[] args) {
-        System.out.println("주문하기 기능입니다");
+        System.out.println("주문하기 기능입니다!!");
     }
 }
