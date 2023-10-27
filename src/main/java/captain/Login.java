ebb131e35be9c2fc59a417672b3cfab547cde496
Minwoo Jhung <minwoorich@naver.com>
2023-10-27 15:50:01 +0900

    [팀장] 첫번째 커밋

---
+++ src/main/java/captain/Login.java
@@ -0,0 +1,7 @@
+package captain;
+
+public class Login {
+    public static void main(String[] args) {
+        System.out.println("로그인 기능입니다");
+    }
+}
---
+++ src/main/java/member/Logout.java
@@ -0,0 +1,7 @@
+package member;
+
+public class Logout {
+    public static void main(String[] args) {
+        System.out.println("로그아웃 기능입니다");
+    }
+}
