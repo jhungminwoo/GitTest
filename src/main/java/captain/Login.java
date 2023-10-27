fe5b9941f92858e34eb1f236d896b9adcbd0fae2
Minwoo Jhung <minwoorich@naver.com>
2023-10-27 16:15:29 +0900

    [팀장] 로그인 기능 수정

--- src/main/java/captain/Login.java
+++ src/main/java/captain/Login.java
@@ -3,5 +3,6 @@ package captain;
 public class Login {
     public static void main(String[] args) {
         System.out.println("로그인 기능입니다");
+        System.out.println("팀장이 로그인 기능을 수정했다!");
     }
 }
